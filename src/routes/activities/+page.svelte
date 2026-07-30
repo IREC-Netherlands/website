ac9e182d639c818ce7ae3d5f8d1f5dde5cca502e
arifyunando <arifyunando@gmail.com>
2026-07-30 21:09:02 +0200

    🚚 add new assets

---
+++ src/routes/activities/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+	import '$lib/styles/pages.css';
+	import ProjectHighlight from '$lib/components/projectHighlight.svelte';
+</script>
+
+<main>
+	<h1>Activities Bulletins</h1>
+	<section class="projects">
+		<ProjectHighlight />
+		<ProjectHighlight />
+		<ProjectHighlight />
+		<ProjectHighlight />
+		<ProjectHighlight />
+		<ProjectHighlight />
+	</section>
+</main>
+
+<style>
+	.projects {
+		display: flex;
+		gap: var(--size-4);
+		flex-wrap: wrap;
+		justify-content: center;
+	}
+</style>
---
+++ src/routes/location/+page.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import '$lib/styles/pages.css';
+</script>
+
+<main>
+	<h1>Location</h1>
+	<div class="locations">
+		<div class="contact-info">
+			<p class="title">International Reformed Evangelical Church in the Netherlands</p>
+			<p>
+				<b> Vierhovenkerk </b>
+				<br />Obrechtstraat 50
+				<br />2625 XN, Delft
+			</p>
+			<br />
+		</div>
+		<iframe
+			title="location"
+			width="800"
+			height="650"
+			style="border:0"
+			loading="lazy"
+			allowfullscreen
+			src="https://www.google.com/maps/embed/v1/search?q=Vierhovenkerk%2C%20Obrechtstraat%2C%20Delft%2C%20Netherlands&key=AIzaSyDL6gW0lwVxvjiV7SZQ3qZ0cGOkiuYZLoU"
+		></iframe>
+	</div>
+</main>
+
+<style>
+	.title {
+		font-weight: var(--font-weight-7);
+	}
+	.locations {
+		flex-direction: row;
+	}
+	.contact-info {
+		max-width: var(--size-15);
+	}
+	p {
+		margin-block: var(--size-2);
+	}
+</style>
